281ea4c6b57f0cd9b4015b7cd04c4959eab4e5a5
Your Name <you@example.com>
2024-08-14 15:56:41 +0000

    spの受信処理も作成(未確認)

--- SP_App/sp_app.cpp
+++ SP_App/sp_app.cpp
@@ -1809,6 +1809,141 @@ int sample_remote_computation(std::string isv_url,
                 exit(1);
             }
         }
+        else if (choice == 2)
+        {
+            print_debug_message("Input your masterpass", INFO);
+            std::string master;
+            std::getline(std::cin, master);
+            print_debug_message("Input the label of the password you want to retrieve", INFO);
+            std::string label;
+            std::getline(std::cin, label);
+
+            // master, label を暗号化して送信
+            uint8_t *plain_send_1 = (uint8_t *)master.c_str();
+            uint8_t *plain_send_2 = (uint8_t *)label.c_str();
+
+            size_t secret_1_len = master.length() + 1;
+            size_t secret_2_len = label.length() + 1;
+
+            uint8_t *iv_send = new uint8_t[12]();
+            uint8_t *tag_send_1 = new uint8_t[16]();
+            uint8_t *tag_send_2 = new uint8_t[16]();
+
+            uint8_t *cipher_send_1 = new uint8_t[secret_1_len]();
+            uint8_t *cipher_send_2 = new uint8_t[secret_2_len]();
+
+            if (generate_nonce(iv_send, 12))
+                return -1;
+
+            // SKで暗号化
+            if (-1 == (aes_128_gcm_encrypt(plain_send_1,
+                                           secret_1_len, sk, iv_send, cipher_send_1, tag_send_1)))
+            {
+                return -1;
+            }
+
+            if (-1 == (aes_128_gcm_encrypt(plain_send_2,
+                                           secret_2_len, sk, iv_send, cipher_send_2, tag_send_2)))
+            {
+                return -1;
+            }
+
+            char *cs1_b64, *cs2_b64;
+            char *ivs_b64;
+            char *tags1_b64, *tags2_b64;
+
+            cs1_b64 = base64_encode<char, uint8_t>(cipher_send_1, secret_1_len);
+            cs2_b64 = base64_encode<char, uint8_t>(cipher_send_2, secret_2_len);
+            ivs_b64 = base64_encode<char, uint8_t>(iv_send, 12);
+            tags1_b64 = base64_encode<char, uint8_t>(tag_send_1, 16);
+            tags2_b64 = base64_encode<char, uint8_t>(tag_send_2, 16);
+
+            json::JSON req_json_obj, res_json_obj;
+            std::string request_json, response_json;
+
+            req_json_obj["ra_context"] = ra_ctx_b64;
+            req_json_obj["cipher1"] = cs1_b64;
+            req_json_obj["cipher2"] = cs2_b64;
+            req_json_obj["iv"] = ivs_b64;
+            req_json_obj["tag1"] = tags1_b64;
+            req_json_obj["tag2"] = tags2_b64;
+
+            request_json = req_json_obj.dump();
+
+            // パスワード取得リクエストを送信
+            auto res = client.Post("/retrieve_password", request_json, "application/json");
+            response_json = res->body;
+            res_json_obj = json::JSON::Load(response_json);
+
+            if (res->status == 500)
+            {
+                char *error_message;
+                size_t error_message_size;
+
+                error_message = base64_decode<char, char>(
+                    (char *)res_json_obj["error_message"].ToString().c_str(), error_message_size);
+
+                print_debug_message(std::string(error_message), ERROR);
+                return -1;
+            }
+            else if (res->status == 401)
+            {
+                print_debug_message("Masterpass is incorrect", ERROR);
+                return -1;
+            }
+            else if (res->status == 200)
+            {
+                // 受信したパスワードの復号処理
+                uint8_t *cipher_result, *plain_result;
+                uint8_t *iv_result, *tag_result;
+                size_t cipher_result_len, tmpsz;
+
+                cipher_result = base64_decode<uint8_t, char>(
+                    (char *)res_json_obj["cipher"].ToString().c_str(), cipher_result_len);
+
+                plain_result = new uint8_t[cipher_result_len]();
+
+                iv_result = base64_decode<uint8_t, char>(
+                    (char *)res_json_obj["iv"].ToString().c_str(), tmpsz);
+
+                if (tmpsz != 12)
+                {
+                    print_debug_message("Invalidly formatted IV received.", ERROR);
+                    return -1;
+                }
+
+                tag_result = base64_decode<uint8_t, char>(
+                    (char *)res_json_obj["tag"].ToString().c_str(), tmpsz);
+
+                if (tmpsz != 16)
+                {
+                    print_debug_message("Invalidly formatted MAC tag received.", ERROR);
+                    return -1;
+                }
+
+                if (-1 == (aes_128_gcm_decrypt(cipher_result,
+                                               cipher_result_len, sk, iv_result, tag_result, plain_result)))
+                {
+                    print_debug_message("Failed to decrypt the password", ERROR);
+                    return -1;
+                }
+
+                // 復号されたパスワードを出力
+                print_debug_message("Retrieved password -> ", INFO);
+                print_debug_message(std::string((char *)plain_result), INFO);
+            }
+            else
+            {
+                std::string message = "Unexpected error while processing password retrieval.";
+                print_debug_message(message, ERROR);
+                exit(1);
+            }
+        }
+        else
+        {
+            print_debug_message("Invalid input", ERROR);
+            return -1;
+        }
     }
     return 0;
 }
